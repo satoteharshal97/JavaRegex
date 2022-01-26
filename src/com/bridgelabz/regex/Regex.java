b4e04b67330115828017189d45e60d27830f084e
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-26 18:55:50 +0530

    Refactor - Changed name

--- src/com/bridgelabz/regex/Regex.java
+++ src/com/bridgelabz/regex/Regex.java
@@ -9,25 +9,9 @@ public class Regex {
     private final static Regex regex = new Regex();
 
     public static void main(String[] args) {
-
-        System.out.println("Welcome to Java Regex");
-        System.out.println("Choose an appropriate option: ");
-        boolean check = true;
-        while (check) {
-            System.out.println("Select:\n" + "1.Check First Name\n" + "2.Exit\n");
-            int option = Integer.parseInt(scanner.nextLine());
-            switch (option) {
-                case 1:
-                    System.out.println("Enter first name:");
-                    regex.checkFirstName(scanner.nextLine());
-                    break;
-                case 2:
-                    check = false;
-                    break;
-                default:
-                    System.out.println("Compile Again!");
-            }
-        }
+        System.out.println("Enter first name:");
+        String firstName = scanner.nextLine();
+        regex.checkFirstName(firstName);
     }
 
     private void checkFirstName(String name) {
